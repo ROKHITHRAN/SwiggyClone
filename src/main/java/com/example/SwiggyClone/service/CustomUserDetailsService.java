cecaa8b29714d7c3ed5832232452324bf723755a
ROKHITHRAN <118052257+ROKHITHRAN@users.noreply.github.com>
2026-03-28 11:56:22 +0530

    Implement JWT authentication with security configuration and user details service; add necessary dependencies

---
+++ src/main/java/com/example/SwiggyClone/service/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.example.SwiggyClone.service;
+
+
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.SwiggyClone.model.User;
+import com.example.SwiggyClone.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        User user = userRepo.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
+}
