d56c0f98e8ded0abfa85d5f7fb899489df148d02
ROKHITHRAN <118052257+ROKHITHRAN@users.noreply.github.com>
2026-03-28 12:23:07 +0530

    Add global exception handler and resource not found exception class

---
+++ src/main/java/com/example/SwiggyClone/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.example.SwiggyClone.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> handleResourceNotFound(ResourceNotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGeneric(Exception ex) {
+        ex.printStackTrace(); // 🔥 ADD THIS
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Something went wrong");
+    }
+}
